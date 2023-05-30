290020cf32485bd406eb611203ac025ba7dd7869
GuiDores <106282415+GuiDores@users.noreply.github.com>
2023-05-30 18:21:37 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,27 @@
+export const dataFake = [
+  {
+    "id": "1",
+    "title": "Half-Life 3 foi anunciado na BGS",
+    "description": "Novo Half-Life foi anunciado neste fim de semana e causando alvoroço nos fãs da série e sim a Valve sabe contar até 3.",
+    "photoCover": "https://sm.ign.com/ign_pt/screenshot/default/quug3on_129p.jpg"
+  },
+  {
+    "id": "2",
+    "title": "Top 10 jogos indes da steam",
+    "description": "Veja os 10 jogos indies mais bem avaliados na steam.",
+    "photoCover": "https://www.gamerinfo.com.br/wp-content/uploads/2022/07/179_Capa_-_Jogos_indies-1.jpg"
+  },
+  {
+    "id": "3",
+    "title": "God of war foi anunciado para pc",
+    "description": "Sony anuncia data de lançamento de god of war para pc nas principais  plataformas digitais.",
+    "photoCover": "https://files.tecnoblog.net/wp-content/uploads/2021/10/god-of-war-pc-steam-epic-games-store.jpg"
+  },
+  {
+    "id": "4",
+    "title": "Bloodborne remake/remaster chegará para pc",
+    "description": "Fromsoftware está trabalhando em um remaster de seu tão aclamado jogo lançado em 2015, o Bloodborne mais uma dúvida assola os players o jogo chegará na steam?",
+    "photoCover": "https://cdn.ome.lt/0GBhQqBi3-33GFl4JFSVTzGf9p4=/1200x630/smart/extras/conteudos/bloodborne-protagonista.png"
+  }
+
+]
